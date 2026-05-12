b3d45ff7d166d35855dbd9a3272a8177e967d5f8
PradeepKumar21-aws <dilleep844@gmail.com>
2026-05-12 12:36:30 +0530

    server.js

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send("Hello from Node.js DevOps Projects 🚀");
+    res.send("Hello from Node.js DevOps Projects from jenkins 🚀");
 });
 
 app.get('/health', (req, res) => {
